97161d37da415d4512fca3dec24428c0d1ee933d
DerLed <48630545+DerLed@users.noreply.github.com>
2021-09-24 16:48:56 +0300

    Update certificate.py

--- certificate.py
+++ certificate.py
@@ -1,23 +1,23 @@
 class Certificate:
-    def __init__(self, thickness):
-        self.metal_grade = thickness[9]  # Марка металла
-        self.gost = None  # Гост материала
-        self.melting_number = None  # Номер плавки
-        self.batch_number = None  # Номер партии
-        self.certificate_number = None  # Номер сертификата
-        self.date_of_certificate = None  # Дата сертификата
-        self.limit_fluidity = None  # Предел текучести
-        self.temporary_resistance = None  # Временное сопротивление
-        self.relative_extension = None  # Относительное удлинение
-        self.relative_narrowing = None  # Относительное сужение
-        self.impact_strength_before_aging = None  # Ударная вязкость до старения
-        self.impact_strength_after_aging = None  # Ударная вязкость после старения
-        self.sample_type = None  # Тип образца
-        self.impact_strength_below_zero = None  # Ударная вязкость ниже ноля
-        self.temperature_below_zero = None  # Температура ниже ноля
-        self.sample_type_below_zero = None  # Тип образка ниже ноля
-        self.thickness = thickness[3]
-        self.additional_data = None  # Доболнительные данные
+    def __init__(self, cert_value):
+        self.thickness = cert_value[3]
+        self.metal_grade = cert_value[9]  # Марка металла
+        self.gost = cert_value[10]  # Гост материала
+        self.melting_number = cert_value[11]  # Номер плавки
+        self.batch_number = cert_value[11]  # Номер партии
+        self.certificate_number = cert_value[12]  # Номер сертификата
+        self.date_of_certificate = cert_value[12]  # Дата сертификата
+        self.limit_fluidity = cert_value[13]  # Предел текучести
+        self.temporary_resistance = cert_value[14]  # Временное сопротивление
+        self.relative_extension = cert_value[15]  # Относительное удлинение
+        self.relative_narrowing = cert_value[16]  # Относительное сужение
+        self.impact_strength_before_aging = cert_value[17]  # Ударная вязкость до старения
+        self.impact_strength_after_aging = cert_value[18]  # Ударная вязкость после старения
+        self.sample_type = cert_value[19]  # Тип образца
+        self.impact_strength_below_zero = cert_value[20]  # Ударная вязкость ниже ноля
+        self.temperature_below_zero = cert_value[21]  # Температура ниже ноля
+        self.sample_type_below_zero = cert_value[22]  # Тип образка ниже ноля
+        self.additional_data = cert_value[23]  # Доболнительные данные
         self.chemical_composition = []  # Хим.состав
 
 
